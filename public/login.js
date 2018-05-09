608f722dd7c03f4bad08ef850e12f59898e4a87f
Vennie Chang <venniechang@yahoo.com>
2018-05-08 23:01:39 -0700

    added publicDirectory for app.get

---
+++ public/login.js
@@ -0,0 +1,5 @@
+function handleLogin() {
+    $('#login').submit(function(event) {
+        event.preventDefault();
+    })
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+//require('dotenv').config();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
 
+const publicDirectory = path.join(__dirname, '../public');
+
+//add another link to get to entries.html
 app.get("/entries", (req, res) => {
-    res.sendFile('../public/entries.html' , {root : __dirname});
+    res.sendFile('entries.html', {root: publicDirectory});
+})
+
+app.get("/add-entry", (req, res) => {
+    res.sendFile('addentry.html', {root: publicDirectory});
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile('login.html', {root: publicDirectory});
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile('register.html', {root: publicDirectory});
+})
+
+app.get("/settings", (req, res) => {
+    res.sendFile('settings.html', {root: publicDirectory});
 })

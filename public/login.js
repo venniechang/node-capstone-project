320769324b93dea503b36a1c20b20a0651fe164e
Vennie Chang <venniechang@yahoo.com>
2018-05-10 11:31:34 -0700

    added JWT

--- public/login.js
+++ public/login.js
@@ -1,5 +1,36 @@
-function handleLogin() {
-    $('#login').submit(function(event) {
+function handleRegistration(){
+    $('#register').submit(function(event){
         event.preventDefault();
+
+        const newUser = {
+            username: $('#new-username').val(),
+            password: $('#new-password').val(),
+            email: $('#new-email').val()
+        }
+
+        if(!$('#new-password').val() == $('#verify-password').val()) {
+            return('Password does not match. Please try again.')     
+        }
+        else {
+            $.post("/api/register", newUser, storeUserData)
+        }
+    })
+}
+
+function storeUserData(data){
+    console.log(data);
+
+}
+
+function handleLogin() {
+    $('.login-button').submit(function(event) {
+        event.preventDefault();
+
+        const user = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        };   
     })
 };
+
+handleRegistration();
--- src/server.js
+++ src/server.js
@@ -5,13 +5,16 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const publicDirectory = path.join(__dirname, '../public');
+const {localStrategy, jwtStrategy } = require('./strategies');
+const {router: userRouter} = require('./routers/userRouter');
+const { PORT, DATABASE_URL } = require('./config');
 
 
+//mongoose.Promise = global.Promise();
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
-
-const publicDirectory = path.join(__dirname, '../public');
 
 //add another link to get to entries.html
 app.get("/entries", (req, res) => {
@@ -33,3 +36,71 @@ app.get("/register", (req, res) => {
 app.get("/settings", (req, res) => {
     res.sendFile('settings.html', {root: publicDirectory});
 })
+
+// CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+      return res.send(204);
+    }
+    next();
+  });
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', userRouter);
+//app.use('/api/auth/', authRouter);
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
+
+
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(databaseUrl, err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app.listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      });
+    });
+  }
+
+  function closeServer() {
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
+  }
+  
+  // if server.js is called directly (aka, with `node server.js`), this block
+  // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+  if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+  }
+  
+  module.exports = { runServer, app, closeServer };
